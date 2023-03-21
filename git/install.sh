4c4f3035f390246f79f7f39632e84f727b058e89
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-21 01:23:57 -0300

    added git installer

---
+++ git/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install Git
+
+#Git:
+	echo "Installing Git."
+	sudo dnf install git
+
+#Git Credential Manager:
+	echo "Installing Git Credential Manager."
+	curl -L https://aka.ms/gcm/linux-install-source.sh | sh
