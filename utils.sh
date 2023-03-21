72ce36b7896eee7d2ff74c0bdf5b3376fea7c648
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-21 14:18:25 -0300

    added new colors to log

--- utils.sh
+++ utils.sh
@@ -3,10 +3,16 @@
 log() {
 	declare -A colors
 
-	colors["red"]="\033[31;1m"
-	colors["yellow"]="\033[33;1m"
-	colors["green"]="\033[32;1m"
-	colors["blue"]="\033[34;1m"
+	colors["black"]="\033[30m"
+	colors["red"]="\033[31m"
+	colors["green"]="\033[32m"
+	colors["yellow"]="\033[33m"
+	colors["blue"]="\033[34m"
+	colors["magenta"]="\033[35m"
+	colors["cyan"]="\033[36m"
+	colors["white"]="\033[37m"
+	colors["default"]="\033[39m"
+
 	colors["end"]="\033[0m"
 
 	echo -e "${colors[$2]} $1 ${colors["end"]}"
