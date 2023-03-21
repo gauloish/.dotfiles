4157a2a88ba45758e14cf6cc6878037e88ee4f26
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-21 17:07:33 -0300

    added wezterm installer

---
+++ wezterm/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+source $HOME/.dotfiles/utils.sh
+
+#----- Install Wezterm
+
+#Wezterm:
+	log "Installing Wezterm." "blue"
+	sudo dnf install https://github.com/wez/wezterm/releases/download/20230320-124340-559cb7b0/wezterm-20230320_124340_559cb7b0-1.fedora37.x86_64.rpm
+
+echo " "
+log "Wezterm Installed." "green"
