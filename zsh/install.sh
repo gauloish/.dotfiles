6c23080ff57c7795650a7ba1c6ae1355d2e657b7
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-21 17:19:36 -0300

    added better logs and change default shell to zshell

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+source $HOME/.dotfiles/utils.sh
+
 #----- Install Zshell
 
 #Zshell:
-	echo "Installing Zshell."
+	log "Installing Zshell." "blue"
 	sudo dnf install zsh
+
+	log "Changing Default Shell to Zshell" "cyan"
+	chsh -s $(which zsh)
+
+echo " "
+log "Zshell Installed." "green"
