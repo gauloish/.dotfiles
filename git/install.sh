a56592f235c0850c82bc2d31af089941705d77fc
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-22 01:20:28 -0300

    test

--- git/install.sh
+++ git/install.sh
@@ -10,7 +10,9 @@ source $HOME/.dotfiles/utils.sh
 
 #Git Credential Manager:
 	log "Installing Git Credential Manager." "blue"
-	curl -L https://aka.ms/gcm/linux-install-source.sh | sh
+	#curl -L https://aka.ms/gcm/linux-install-source.sh | sh
+
+rm $HOME/.dotfiles/dotnet-install.sh
 
 echo " "
 log "Git and All Dependencies Installed." "green"
