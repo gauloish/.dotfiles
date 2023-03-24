299d6b9e2fad370b1459ade61d86fd2a533f5677
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-03-24 03:26:41 -0300

    added ligatures

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,8 @@ if wezterm.config_builder then
 end
 
 config["font"] = wezterm.font("JetBrainsMono Nerd Font")
-config["font_size"] = 10.0
+config["font_size"] = 10
+config["harfbuzz_features"] = { "calt=1", "clig=1", "liga=1" }
 config["color_scheme"] = "Tokyo Night Storm (Gogh)"
 config["default_prog"] = { "/bin/zsh", "-l" }
 
