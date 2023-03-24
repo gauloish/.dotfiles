72f021dd5f969a76d69bb218c985f9230b851b96
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-03-24 03:27:00 -0300

    changed wezterm icon

--- wezterm/install.sh
+++ wezterm/install.sh
@@ -8,5 +8,10 @@ source $HOME/.dotfiles/utils.sh
 	log "Installing Wezterm." "blue"
 	sudo dnf install -y https://github.com/wez/wezterm/releases/download/20230320-124340-559cb7b0/wezterm-20230320_124340_559cb7b0-1.fedora37.x86_64.rpm
 
+	sudo rm -f /usr/share/icons/hicolor/128x128/apps/org.wezfurlong.wezterm.png
+	sudo mv $HOME/.dotfiles/wezterm/icon.png $HOME/.dotfiles/wezterm/org.wezfurlong.wezterm.png
+	sudo cp $HOME/.dotfiles/wezterm/org.wezfurlong.wezterm.png /usr/share/icons/hicolor/128x128/apps
+	sudo mv $HOME/.dotfiles/wezterm/org.wezfurlong.wezterm.png $HOME/.dotfiles/wezterm/icon.png 
+
 echo " "
 log "Wezterm Installed." "green"
