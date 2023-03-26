31558d8f96a5a46a52285bacf100efe69756245f
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-03-26 18:12:31 -0300

    added  variable

--- wezterm/install.sh
+++ wezterm/install.sh
@@ -9,9 +9,9 @@ source $HOME/.dotfiles/utils.sh
 	sudo dnf install -y https://github.com/wez/wezterm/releases/download/20230320-124340-559cb7b0/wezterm-20230320_124340_559cb7b0-1.fedora37.x86_64.rpm
 
 	sudo rm -f /usr/share/icons/hicolor/128x128/apps/org.wezfurlong.wezterm.png
-	sudo mv $HOME/.dotfiles/wezterm/icon.png $HOME/.dotfiles/wezterm/org.wezfurlong.wezterm.png
-	sudo cp $HOME/.dotfiles/wezterm/org.wezfurlong.wezterm.png /usr/share/icons/hicolor/128x128/apps
-	sudo mv $HOME/.dotfiles/wezterm/org.wezfurlong.wezterm.png $HOME/.dotfiles/wezterm/icon.png 
+	sudo mv $REPO/wezterm/icon.png $REPO/wezterm/org.wezfurlong.wezterm.png
+	sudo cp $REPO/wezterm/org.wezfurlong.wezterm.png /usr/share/icons/hicolor/128x128/apps
+	sudo mv $REPO/wezterm/org.wezfurlong.wezterm.png $REPO/wezterm/icon.png 
 
 	echo " "
 	log "Wezterm Installed." "green"
@@ -21,7 +21,7 @@ source $HOME/.dotfiles/utils.sh
 #Link:
 	log "Linking Wezterm Configuration!" "yellow"
 	rm $HOME/.wezterm.lua
-	ln -s $HOME/.dotfiles/wezterm/.wezterm.lua $HOME/.wezterm.lua
+	ln -s $REPO/wezterm/.wezterm.lua $HOME/.wezterm.lua
 
 	echo " "
 	log "Wezterm Configured." "green"
