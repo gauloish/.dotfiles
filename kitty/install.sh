75b0aa064fb570a80d96bc417dbe114dcb7e120a
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-03-26 18:03:23 -0300

    added  and  variables

--- kitty/install.sh
+++ kitty/install.sh
@@ -9,16 +9,16 @@ source $HOME/.dotfiles/utils.sh
 	sudo dnf install -y kitty
 
 	sudo rm -f /usr/share/icons/hicolor/64x64/apps/kitty.png
-	sudo cp $HOME/.dotfiles/kitty/icons/64x64/kitty.png /usr/share/icons/hicolor/64x64/apps/kitty.png
+	sudo cp $REPO/kitty/icons/64x64/kitty.png /usr/share/icons/hicolor/64x64/apps/kitty.png
 
 	sudo rm -f /usr/share/icons/hicolor/128x128/apps/kitty.png
-	sudo cp $HOME/.dotfiles/kitty/icons/128x128/kitty.png /usr/share/icons/hicolor/128x128/apps/kitty.png
+	sudo cp $REPO/kitty/icons/128x128/kitty.png /usr/share/icons/hicolor/128x128/apps/kitty.png
 
 	sudo rm -f /usr/share/icons/hicolor/256x256/apps/kitty.png
-	sudo cp $HOME/.dotfiles/kitty/icons/256x256/kitty.png /usr/share/icons/hicolor/256x256/apps/kitty.png
+	sudo cp $REPO/kitty/icons/256x256/kitty.png /usr/share/icons/hicolor/256x256/apps/kitty.png
 
 	sudo rm -f /usr/share/icons/hicolor/scalable/apps/kitty.svg
-	sudo cp $HOME/.dotfiles/kitty/icons/scalable/kitty.svg /usr/share/icons/hicolor/scalable/apps/kitty.svg
+	sudo cp $REPO/kitty/icons/scalable/kitty.svg /usr/share/icons/hicolor/scalable/apps/kitty.svg
 
 	echo " "
 	log "Kitty Installed." "green"
@@ -27,9 +27,9 @@ source $HOME/.dotfiles/utils.sh
 
 #Link:
 	log "Linking Kitty Configuration!" "yellow"
-	rm $HOME/.config/kitty/kitty.conf
-	mkdir $HOME/.config/kitty/
-	ln -s $HOME/.dotfiles/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+	rm $CONF/kitty/kitty.conf
+	mkdir $CONF/kitty/
+	ln -s $REPO/kitty/kitty.conf $CONF/kitty/kitty.conf
 
 	echo " "
 	log "Kitty Configured." "green"
