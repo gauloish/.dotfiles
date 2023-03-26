594e536e39e02fbd24e75e33ecb2bed819dedf90
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-03-26 17:59:38 -0300

    added  variable

--- git/install.sh
+++ git/install.sh
@@ -12,7 +12,7 @@ source $HOME/.dotfiles/utils.sh
 	log "Installing Git Credential Manager." "blue"
 	#curl -L https://aka.ms/gcm/linux-install-source.sh | sh
 
-	rm $HOME/.dotfiles/dotnet-install.sh
+	rm $REPO/dotnet-install.sh
 
 	echo " "
 	log "Git and All Dependencies Installed." "green"
@@ -22,7 +22,7 @@ source $HOME/.dotfiles/utils.sh
 #Link:
 	log "Linking Git Configuration!" "yellow"
 	rm $HOME/.gitconfig
-	ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
+	ln -s $REPO/git/.gitconfig $HOME/.gitconfig
 
 	echo " "
 	log "Git Configured." "green"
