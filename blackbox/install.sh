b9a7e0b25fe152ddad6ac3de4fe47a6fda9383a2
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-03-27 02:01:43 -0300

    uncomment some lines

--- blackbox/install.sh
+++ blackbox/install.sh
@@ -10,16 +10,16 @@ FILE=com.raggesilver.BlackBox
 
 #Dependencies:
 	log "Setup Flatpak to Use Flathub." "blue"
-	# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #Blackbox:
 	log "Installing BlackBox." "blue"
-	# flatpak install flathub com.raggesilver.BlackBox
-
-	echo " "
-	log "BlackBox Installed." "green"
+	flatpak install flathub com.raggesilver.BlackBox
 
 	log "Changing BlackBox Icons." "blue"
 
 	sudo cp $HOME/.dotfiles/blackbox/icons/${FILE}.svg $HEAD/x86_64/stable/active/export/${FORK}/${FILE}.svg
 	sudo cp $HOME/.dotfiles/blackbox/icons/${FILE}.svg $HEAD/x86_64/stable/active/files/${FORK}/${FILE}.svg
+
+	echo " "
+	log "BlackBox Installed." "green"
