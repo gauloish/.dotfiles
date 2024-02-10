b72bc0e5fd87461155b0d61b45571011b3bf6189
Gaulöis <125079293+gauloish@users.noreply.github.com>
2024-02-10 16:55:38 -0300

    added script to change theme

---
+++ orchis/theme.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+source $HOME/.dotfiles/utils.sh
+
+if [ "$1" == "Grey" ] || [ "$1" == "Dracula" ] || [ "$1" == "Nord" ]; then
+  rm -rf $CONF/gtk-4.0/assets
+  rm -rf $CONF/gtk-4.0/gtk.css
+  rm -rf $CONF/gtk-4.0/gtk-dark.css
+
+  theme=Orchis-Grey-Dark
+
+  if [ "$1" == "Dracula" ] || [ "$1" == "Nord" ]; then
+    theme=$theme-$1
+  fi
+
+  ln -s $HOME/.themes/$theme/gtk-4.0/assets $CONF/gtk-4.0/assets
+  ln -s $HOME/.themes/$theme/gtk-4.0/gtk.css $CONF/gtk-4.0/gtk.css
+  ln -s $HOME/.themes/$theme/gtk-4.0/gtk-dark.css $CONF/gtk-4.0/gtk-dark.css
+else
+  log "Invalid Theme!!!" "red"
+fi
