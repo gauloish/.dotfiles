d3a8475cad7074fcc440c1e64f3a2ac53ff0d46f
Gaulöis <125079293+gauloish@users.noreply.github.com>
2024-02-11 01:49:41 -0300

    added installer to grub themes

---
+++ grub/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+source $HOME/.dotfiles/utils.sh
+
+#----- Install Grub Theme
+
+if [ "$1" == "1080p" ] || [ "$1" == "ultrawide" ]; then
+    rm -r -f $REPO/grub/package
+    mkdir $REPO/grub/package
+
+    git -C $REPO/grub/package clone https://github.com/vinceliuice/grub2-themes.git
+    sudo sh $REPO/grub/package/grub2-themes/install.sh --remove stylish
+    sudo sh $REPO/grub/package/grub2-themes/install.sh --theme stylish --icon white --screen $1
+
+    rm -r -f $REPO/grub/package
+fi
