7a7c1f6854acc13806521b51b702e0175a77f728
Gaulöis <125079293+gauloish@users.noreply.github.com>
2024-02-12 04:48:03 -0300

    link starship config

--- starship/install.ps1
+++ starship/install.ps1
@@ -1,4 +1,8 @@
+. "$HOME\.dotfiles\utils.ps1"
+
 # Starship:
-    echo "Installing Starship."
+	echo "Installing Starship."
 
 	scoop install starship
+
+ 	mklink "$REPO\starship\starship.toml" "$CONF\starchip.toml"
