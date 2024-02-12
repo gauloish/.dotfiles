0611436330dd8274cb167b7c0ad2b78a28b4e992
Gaulöis <125079293+gauloish@users.noreply.github.com>
2024-02-12 03:15:18 -0300

    changed install.ps1 to use setup.ps1 and repository files

--- install.ps1
+++ install.ps1
@@ -19,11 +19,11 @@ $URL = "https://github.com/gauloish/.dotfiles"
 
 	scoop bucket add main
 
-	scoop install main/zip
-	scoop install main/unzip
-	scoop install main/wget
-	scoop install main/curl
-	scoop install main/git
+	scoop install zip
+	scoop install unzip
+	scoop install wget
+	scoop install curl
+	scoop install git
 
 # Setting Git:
 	rm -Force .gitconfig
@@ -32,16 +32,8 @@ $URL = "https://github.com/gauloish/.dotfiles"
 	git config --global init.defaultBranch main
 
 	# Git User
-	git config --global user.email 125079293+gauloish@users.noreply.github.com
-	git config --global user.name gauloish
-
-	# Git Credential
-	git config --global credential.helper manager
-	git config --global credential.credentialStore cache
-	git config --global credential.cacheOptions --timeout 3600
-
-	# Git Core
-	git config --global core.editor nvim
+	git config --global user.email temp
+	git config --global user.name temp
 
 # Git Credential Manager:
 	echo "Installing Git Credential Manager."
@@ -50,55 +42,15 @@ $URL = "https://github.com/gauloish/.dotfiles"
 
 	scoop install extras/git-credential-manager
 
-# Nerd Fonts:
-	echo "Installing Git Credential Manager."
-	
-	scoop bucket add nerd-fonts
-
-	scoop install nerd-fonts/Cascadia-Code
-	scoop install nerd-fonts/FantasqueSansMono-NF
-	scoop install nerd-fonts/JetBrains-Mono
-	scoop install nerd-fonts/Victor-Mono
-
-# Gcc:
-	scoop install main/gcc
-
-# Python:
-	scoop install main/python
-	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py; python get-pip.py
-	pip install pynvim
-	pip install yarp
-
-# Node:
-	scoop install main/nodejs
-
-# Rust:
-	scoop install main/rust
-
-# Lua:
-	scoop install main/lua
-	scoop install main/luarocks
-
-# Utilitary:
-	scoop install main/grep
-	scoop install main/ripgrep
-	scoop install main/fd
-
-# Starship:
-	scoop install main/starship
-
-# Neovim:
-	scoop install main/neovim
-
 # Create Dotfiles Directory:
-	#rm -Force $REPO
-	#mkdir .dotfiles
-	#cd .dotfiles
+	rm -Force $REPO
+	mkdir .dotfiles
+	cd .dotfiles
 
 # Initialize Git Repository:
-	#git init
- 	#git pull $URL
- 	#git remote add origin $URL
+	git init
+ 	git pull $URL
+ 	git remote add origin $URL
 
 # Running Setup:
-   	#sh $REPO/setup.sh
+   	.\setup.ps1
