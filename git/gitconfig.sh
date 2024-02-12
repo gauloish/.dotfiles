8d35f29a496829ac3610c94aedf86d8c214f2baf
Gaulöis <125079293+gauloish@users.noreply.github.com>
2024-02-12 02:37:25 -0300

    added gitconfig.sh to run my git config

---
+++ git/gitconfig.sh
@@ -0,0 +1,14 @@
+# Git Init
+git config --global init.defaultBranch main
+
+# Git User
+git config --global user.email 125079293+gauloish@users.noreply.github.com
+git config --global user.name gauloish
+
+# Git Credential
+git config --global credential.helper manager
+git config --global credential.credentialStore cache
+git config --global credential.cacheOptions --timeout 3600
+
+# Git Core
+git config --global core.editor nvim
