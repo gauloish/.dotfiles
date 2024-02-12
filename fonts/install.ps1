408a4f264fb9a1dddf8f1099457d33ccfb8ac706
Gaulöis <125079293+gauloish@users.noreply.github.com>
2024-02-12 02:29:34 -0300

    added install.ps1 to install nerd fonts in windows

---
+++ fonts/install.ps1
@@ -0,0 +1,9 @@
+# Nerd Fonts:
+	echo "Installing Git Credential Manager."
+	
+	scoop bucket add nerd-fonts
+
+	scoop install nerd-fonts/Cascadia-Code
+	scoop install nerd-fonts/FantasqueSansMono-NF
+	scoop install nerd-fonts/JetBrains-Mono
+	scoop install nerd-fonts/Victor-Mono
