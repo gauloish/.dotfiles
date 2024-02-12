b664f474dadca981fa39cf9588f8f297ca1bc4a6
gauloish <125079293+gauloish@users.noreply.github.com>
2024-02-12 15:48:12 -0300

    fixed theme script

--- orchis/theme.sh
+++ orchis/theme.sh
@@ -3,21 +3,23 @@
 source $HOME/.dotfiles/utils.sh
 
 if [ "$1" == "Grey" ] || [ "$1" == "Dracula" ] || [ "$1" == "Nord" ]; then
-  mkdir $CONF/gtk-4.0
+    if [ ! -d "$CONF/gtk-4.0/" ]; then
+        mkdir $CONF/gtk-4.0
+    fi
 
-  rm -rf $CONF/gtk-4.0/assets
-  rm -rf $CONF/gtk-4.0/gtk.css
-  rm -rf $CONF/gtk-4.0/gtk-dark.css
+    rm -rf $CONF/gtk-4.0/assets
+    rm -rf $CONF/gtk-4.0/gtk.css
+    rm -rf $CONF/gtk-4.0/gtk-dark.css
 
-  theme=Orchis-Grey-Dark
+    theme=Orchis-Grey-Dark
 
-  if [ "$1" == "Dracula" ] || [ "$1" == "Nord" ]; then
+    if [ "$1" == "Dracula" ] || [ "$1" == "Nord" ]; then
     theme=$theme-$1
-  fi
+    fi
 
-  ln -s $HOME/.themes/$theme/gtk-4.0/assets $CONF/gtk-4.0/assets
-  ln -s $HOME/.themes/$theme/gtk-4.0/gtk.css $CONF/gtk-4.0/gtk.css
-  ln -s $HOME/.themes/$theme/gtk-4.0/gtk-dark.css $CONF/gtk-4.0/gtk-dark.css
+    ln -s $HOME/.themes/$theme/gtk-4.0/assets $CONF/gtk-4.0/assets
+    ln -s $HOME/.themes/$theme/gtk-4.0/gtk.css $CONF/gtk-4.0/gtk.css
+    ln -s $HOME/.themes/$theme/gtk-4.0/gtk-dark.css $CONF/gtk-4.0/gtk-dark.css
 else
-  log "Invalid Theme!!!" "red"
+    log "Invalid Theme!!!" "red"
 fi
