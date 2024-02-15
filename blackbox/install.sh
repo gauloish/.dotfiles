cbcb37c7334398090ce48f0c960f66ecc4ad630b
gauloish <125079293+gauloish@users.noreply.github.com>
2024-02-15 13:47:49 -0300

    tried fixed copy of icon to blackbox

--- blackbox/install.sh
+++ blackbox/install.sh
@@ -18,8 +18,8 @@ FILE=com.raggesilver.BlackBox
 
 	log "Changing BlackBox Icons." "blue"
 
-	sudo cp $HOME/.dotfiles/blackbox/icons/${FILE}.svg $HEAD/x86_64/stable/active/export/${FORK}/${FILE}.svg
-	sudo cp $HOME/.dotfiles/blackbox/icons/${FILE}.svg $HEAD/x86_64/stable/active/files/${FORK}/${FILE}.svg
+	sudo cp $HOME/.dotfiles/blackbox/icons/${FILE}.svg $HEAD/current/active/export/${FORK}/${FILE}.svg
+	sudo cp $HOME/.dotfiles/blackbox/icons/${FILE}.svg $HEAD/current/active/files/${FORK}/${FILE}.svg
 
 	echo " "
 	log "BlackBox Installed." "green"
