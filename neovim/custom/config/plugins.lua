3d3705b741372112fc019597d930c1418a4a1133
gauloish <125079293+gauloish@users.noreply.github.com>
2024-02-17 01:51:21 -0300

    added wilder plugin setup

--- neovim/custom/config/plugins.lua
+++ neovim/custom/config/plugins.lua
@@ -22,6 +22,60 @@ local plugins = {
         "hrsh7th/nvim-cmp",
         lazy = false,
     },
+    {
+        "gauloish/wilder.nvim",
+        lazy = false,
+        config = function()
+            local wilder = require("wilder")
+
+            wilder.setup({
+                modes = { ":" },
+                next_key = "<tab>",
+                previous_key = "<s-tab>",
+                accept_key = "<down>",
+                reject_key = "<up>",
+            })
+
+            wilder.set_option(
+                "renderer",
+                wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+                    mode = "float",
+                    ellipsis = "...",
+                    left = {
+                        " ",
+                        wilder.popupmenu_devicons(),
+                    },
+                    right = {
+                        " ",
+                        wilder.popupmenu_scrollbar(),
+                    },
+                    highlighter = wilder.basic_highlighter(),
+                    highlights = {
+                        border = "CmpBorder",
+                        default = "CmpPmenu",
+                        selected = "CmpSel",
+                        accent = "FloatBorder",
+                        selected_accent = "CmpSel",
+                    },
+                    border = "rounded",
+                    min_width = "20%",
+                    max_width = "60%",
+                    min_height = "0%",
+                    max_height = "40%",
+                }))
+            )
+
+            wilder.set_option("pipeline", {
+                wilder.branch(
+                    wilder.cmdline_pipeline({
+                        language = "vim",
+                        fuzzy = 1,
+                    }),
+                    wilder.vim_search_pipeline()
+                ),
+            })
+        end,
+    }
 }
 
 return plugins
