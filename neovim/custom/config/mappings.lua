5eb47081b14a904825a6debb5c13c4427a4f8cf3
gauloish <125079293+gauloish@users.noreply.github.com>
2024-02-17 16:23:17 -0300

    added neogen mappings

--- neovim/custom/config/mappings.lua
+++ neovim/custom/config/mappings.lua
@@ -2,9 +2,7 @@ local mappings = {}
 
 local options = {
     move = {silent = false, noremap = true},
-    copy = {silent = false, noremap = true},
-    cut = {silent = false, noremap = true},
-    delete = {silent = false, noremap = true},
+    documentation = {silent = false, noremap = true},
 }
 
 mappings.move = {
@@ -19,6 +17,15 @@ mappings.move = {
         ["<a-j>"] = {[[<Down>]], "Move cursor to down", opts = options.move},
         ["<a-k>"] = {[[<Up>]], "Move cursor to up", opts = options.move},
         ["<a-l>"] = {[[<Right>]], "Move cursor to right", opts = options.move},
+    },
+}
+
+mappings.documentation = {
+    n = {
+        ["<a-d>f"] = {function() require("neogen").generate({type = "func"}) end, "Generate function documentation", opts = options.documentation},
+        ["<a-d>c"] = {function() require("neogen").generate({type = "class"}) end, "Generate class documentation", opts = options.documentation},
+        ["<a-d>t"] = {function() require("neogen").generate({type = "type"}) end, "Generate type documentation", opts = options.documentation},
+        ["<a-d>d"] = {function() require("neogen").generate({type = "file"}) end, "Generate file documentation", opts = options.documentation},
     },
 }
 
