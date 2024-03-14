2c9223aea7d4a8d40b6ad43db136db9c247972cf
gauloish <125079293+gauloish@users.noreply.github.com>
2024-03-14 11:59:22 -0300

    fixed the module names to require in init

--- neovim/custom/init.lua
+++ neovim/custom/init.lua
@@ -1,16 +1,2 @@
-local opt = vim.opt
-
-opt.clipboard = ""
-opt.cursorline = true
-
--- Indenting
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.smartindent = true
-opt.tabstop = 4
-opt.softtabstop = 4
-
--- Numbers
-opt.number = true
-opt.relativenumber = true
-opt.numberwidth = 2
+require("custom.config.commands")
+require("custom.config.options")
