4431fdc0e597aadb3df6d8045423ec8cc4a54d74
gauloish <125079293+gauloish@users.noreply.github.com>
2024-03-14 12:04:11 -0300

    fixed some problems with mappings

--- neovim/custom/config/mappings.lua
+++ neovim/custom/config/mappings.lua
@@ -3,29 +3,50 @@ local mappings = {}
 local options = {
     move = {silent = false, noremap = true},
     documentation = {silent = false, noremap = true},
+    general = {silent = false, noremap = true},
+}
+
+mappings.disabled = {
+    n = {
+        ["<A-h>"] = "",
+        ["<A-j>"] = "",
+        ["<A-k>"] = "",
+        ["<A-l>"] = "",
+
+        ["<C-h>"] = "",
+        ["<C-j>"] = "",
+        ["<C-k>"] = "",
+        ["<C-l>"] = "",
+    },
 }
 
 mappings.move = {
     n = {
-        ["<a-h>"] = {[[<c-w>h]], "Move cursor to left window", opts = options.move},
-        ["<a-j>"] = {[[<c-w>j]], "Move cursor to down window", opts = options.move},
-        ["<a-k>"] = {[[<c-w>k]], "Move cursor to up window", opts = options.move},
-        ["<a-l>"] = {[[<c-w>l]], "Move cursor to right window", opts = options.move},
+        ["<A-h>"] = {[[<c-w>h]], "Move cursor to left window", opts = options.move},
+        ["<A-j>"] = {[[<c-w>j]], "Move cursor to down window", opts = options.move},
+        ["<A-k>"] = {[[<c-w>k]], "Move cursor to up window", opts = options.move},
+        ["<A-l>"] = {[[<c-w>l]], "Move cursor to right window", opts = options.move},
     },
     i = {
-        ["<a-h>"] = {[[<Left>]], "Move cursor to left", opts = options.move},
-        ["<a-j>"] = {[[<Down>]], "Move cursor to down", opts = options.move},
-        ["<a-k>"] = {[[<Up>]], "Move cursor to up", opts = options.move},
-        ["<a-l>"] = {[[<Right>]], "Move cursor to right", opts = options.move},
+        ["<A-h>"] = {[[<Left>]], "Move cursor to left", opts = options.move},
+        ["<A-j>"] = {[[<Down>]], "Move cursor to down", opts = options.move},
+        ["<A-k>"] = {[[<Up>]], "Move cursor to up", opts = options.move},
+        ["<A-l>"] = {[[<Right>]], "Move cursor to right", opts = options.move},
     },
 }
 
 mappings.documentation = {
     n = {
-        ["<a-d>f"] = {function() require("neogen").generate({type = "func"}) end, "Generate function documentation", opts = options.documentation},
-        ["<a-d>c"] = {function() require("neogen").generate({type = "class"}) end, "Generate class documentation", opts = options.documentation},
-        ["<a-d>t"] = {function() require("neogen").generate({type = "type"}) end, "Generate type documentation", opts = options.documentation},
-        ["<a-d>d"] = {function() require("neogen").generate({type = "file"}) end, "Generate file documentation", opts = options.documentation},
+        ["<A-d>f"] = {function() require("neogen").generate({type = "func"}) end, "Generate function documentation", opts = options.documentation},
+        ["<A-d>c"] = {function() require("neogen").generate({type = "class"}) end, "Generate class documentation", opts = options.documentation},
+        ["<A-d>t"] = {function() require("neogen").generate({type = "type"}) end, "Generate type documentation", opts = options.documentation},
+        ["<A-d>d"] = {function() require("neogen").generate({type = "file"}) end, "Generate file documentation", opts = options.documentation},
+    },
+}
+
+mappings.general = {
+    n = {
+        -- ["<leader>st"] = {"NvSettings", "Open settings file", opts = options.general},
     },
 }
 
