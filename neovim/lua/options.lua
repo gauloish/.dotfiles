3e354319f77a9df7af0954b36b750b223548caf2
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-04 15:34:54 -0300

    changed options to neovim config

--- neovim/lua/options.lua
+++ neovim/lua/options.lua
@@ -1,6 +1,19 @@
 require "nvchad.options"
 
--- add yours here!
+local option = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+option.number = true
+option.relativenumber = true
+option.cursorline = true
+option.cursorcolumn = false
+option.showmode = false
+
+option.expandtab = false
+option.tabstop = 4
+option.softtabstop = 4
+option.shiftwidth = 4
+
+option.autoindent = true
+option.smartindent = true
+
+option.signcolumn = "yes"
