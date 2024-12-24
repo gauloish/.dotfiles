24d99db3f7640dcbdaec55c044352234ae408d66
gauloish <125079293+gauloish@users.noreply.github.com>
2024-12-24 00:01:34 -0300

    added: install and setup wilder neovim plugin

---
+++ neovim/nvim/lua/plugins/packages/wilder.lua
@@ -0,0 +1,52 @@
+return {
+	"gauloish/wilder.nvim",
+	build = ":UpdateRemotePlugins",
+	config = function()
+		local wilder = require("wilder")
+
+		wilder.setup({
+			modes = { ":", "/", "?" },
+			next_key = "<tab>",
+			previous_key = "<s-tab>",
+			accept_key = "<down>",
+			reject_key = "<up>",
+		})
+
+		wilder.set_option(
+			"renderer",
+			wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+				mode = "float",
+				ellipsis = "...",
+				left = {
+					" ",
+					wilder.popupmenu_devicons({
+						get_hl = function(context, name, directory, icon)
+							return "Normal"
+						end,
+					}),
+				},
+				right = {
+					" ",
+					wilder.popupmenu_scrollbar(),
+				},
+				highlighter = wilder.basic_highlighter(),
+				highlights = {
+					-- TODO: fix highlights for wilder windows
+					border = "Normal",
+					default = "Normal", -- WildDefault",
+					selected = "CursorLine",
+					accent = "Normal", -- WildAccent",
+					selected_accent = "Normal", -- WildSelectedAccent",
+				},
+				border = "rounded",
+				prompt_border = { "│", "─", "│" }, --{ "╭", "─", "╮" },
+				min_width = "20%",
+				max_width = "80%",
+				min_height = "0%",
+				max_height = "60%",
+				prompt_position = "bottom",
+				reverse = false,
+			}))
+		)
+	end,
+}
