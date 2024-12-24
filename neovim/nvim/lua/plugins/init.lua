b4364857d030d617a52b8daa8a63f819a2805971
gauloish <125079293+gauloish@users.noreply.github.com>
2024-12-23 21:47:05 -0300

    added: added telescope plugin for neovim

--- neovim/nvim/lua/plugins/init.lua
+++ neovim/nvim/lua/plugins/init.lua
@@ -29,6 +29,7 @@ vim.opt.rtp:prepend(path)
 
 require("lazy").setup({
 		require("plugins.packages.treesitter"),
+		require("plugins.packages.telescope"),
 		require("plugins.packages.autopairs"),
 		require("plugins.packages.cmp"),
 		require("plugins.packages.ibl"),
