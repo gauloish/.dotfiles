b54040a76f3970cd852c62da63de00fbf54b1ffa
gauloish <125079293+gauloish@users.noreply.github.com>
2025-01-04 04:48:32 -0300

    feat: lazy load color schemes

--- neovim/nvim/lua/plugins/init.lua
+++ neovim/nvim/lua/plugins/init.lua
@@ -1,16 +1,16 @@
 -- Plugins Setup
 require("plugins.lazy").setup({
 	-- Plugins
+	require("plugins.utility.schemes"),
 	require("plugins.utility.treesitter"),
 	require("plugins.utility.telescope"),
-	require("plugins.utility.schemes"),
 	require("plugins.utility.autopairs"),
 	require("plugins.utility.cmp"),
 	require("plugins.utility.indent-blankline"),
 	require("plugins.utility.wilder"),
 	require("plugins.utility.neogen"),
 	require("plugins.utility.comment"),
-	-- require("plugins.utility.gitsigns"),
+	require("plugins.utility.gitsigns"),
 	require("plugins.utility.web-devicons"),
 	require("plugins.utility.feline"),
 	require("plugins.utility.colorizer"),
--- neovim/nvim/lua/plugins/schemes/gruvbox.lua
+++ neovim/nvim/lua/plugins/schemes/gruvbox.lua
@@ -1,4 +1,5 @@
 return {
 	"sainnhe/gruvbox-material",
 	name = "gruvbox.nvim",
+	lazy = true,
 }
