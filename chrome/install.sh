ee325cbd6099f6243d035d1376ea692631885aaf
gauloish <125079293+gauloish@users.noreply.github.com>
2025-01-06 18:43:03 -0300

    feat: using dnf5 in fedora 41

--- chrome/install.sh
+++ chrome/install.sh
@@ -7,7 +7,6 @@ source $HOME/.dotfiles/utils.sh
 #Chrome:
 	log "Installing Chrome." "blue"
 	sudo dnf install -y fedora-workstation-repositories
-	sudo dnf config-manager --set-enabled google-chrome
 	sudo dnf install -y google-chrome-stable
 
 echo " "
