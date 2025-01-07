fbda3c14434c801db7378a2962df928cfd2afd50
gauloish <125079293+gauloish@users.noreply.github.com>
2025-01-07 20:54:24 -0300

    feat: change 'neogen.nvim' keymaps

--- neovim/nvim/lua/plugins/utility/neogen.lua
+++ neovim/nvim/lua/plugins/utility/neogen.lua
@@ -1,15 +1,15 @@
 return {
-	"danymat/neogen", 
+	"danymat/neogen",
 	config = function()
 		require("neogen").setup({
 			snippet_engine = "luasnip",
 			placeholders_hl = "None",
 		})
 
-		vim.keymap.set("n", "<a-d>d", ":Neogen<cr>", { silent = true, desc = "Generate current documentation" })
-		vim.keymap.set("n", "<a-d>f", ":Neogen func<cr>", { silent = true, desc = "Generate function documentation" })
-		vim.keymap.set("n", "<a-d>c", ":Neogen class<cr>", { silent = true, desc = "Generate class documentation" })
-		vim.keymap.set("n", "<a-d>t", ":Neogen type<cr>", { silent = true, desc = "Generate type documentation" })
-		vim.keymap.set("n", "<a-d>a", ":Neogen file<cr>", { silent = true, desc = "Generate file documentation" })
+		vim.keymap.set("n", "<leader>dd", ":Neogen<cr>", { silent = true, desc = "Generate current documentation" })
+		vim.keymap.set("n", "<leader>df", ":Neogen func<cr>", { silent = true, desc = "Generate function documentation" })
+		vim.keymap.set("n", "<leader>dc", ":Neogen class<cr>", { silent = true, desc = "Generate class documentation" })
+		vim.keymap.set("n", "<leader>dt", ":Neogen type<cr>", { silent = true, desc = "Generate type documentation" })
+		vim.keymap.set("n", "<leader>da", ":Neogen file<cr>", { silent = true, desc = "Generate file documentation" })
 	end,
 }
