99bf21c60c365d5695ce42cfcc8dfd1c2406d0f4
gauloish <125079293+gauloish@users.noreply.github.com>
2025-01-09 04:29:19 -0300

    refactor: code more organized

--- neovim/nvim/lua/plugins/utility/wilder.lua
+++ neovim/nvim/lua/plugins/utility/wilder.lua
@@ -14,38 +14,40 @@ return {
 
 		wilder.set_option(
 			"renderer",
-			wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-				mode = "float",
-				ellipsis = "...",
-				left = {
-					" ",
-					wilder.popupmenu_devicons({
-						get_hl = function(context, name, directory, icon)
-							return "Normal"
-						end,
-					}),
-				},
-				right = {
-					" ",
-					wilder.popupmenu_scrollbar(),
-				},
-				highlighter = wilder.basic_highlighter(),
-				highlights = {
-					border = "WildBorder",
-					default = "WildDefault",
-					selected = "WildSelected",
-					accent = "WildAccent",
-					selected_accent = "WildSelectedAccent",
-				},
-				border = "rounded",
-				prompt_border = { "│", "─", "│" }, --{ "╭", "─", "╮" },
-				min_width = "20%",
-				max_width = "80%",
-				min_height = "0%",
-				max_height = "60%",
-				prompt_position = "bottom",
-				reverse = false,
-			}))
+			wilder.popupmenu_renderer(
+				wilder.popupmenu_border_theme({
+					mode = "float",
+					ellipsis = "...",
+					left = {
+						" ",
+						wilder.popupmenu_devicons({
+							get_hl = function(_, _, _, _)
+								return "Normal"
+							end,
+						}),
+					},
+					right = {
+						" ",
+						wilder.popupmenu_scrollbar(),
+					},
+					highlighter = wilder.basic_highlighter(),
+					highlights = {
+						border = "WildBorder",
+						default = "WildDefault",
+						selected = "WildSelected",
+						accent = "WildAccent",
+						selected_accent = "WildSelectedAccent",
+					},
+					border = "rounded",
+					prompt_border = { "│", "─", "│" }, --{ "╭", "─", "╮" },
+					min_width = "20%",
+					max_width = "80%",
+					min_height = "0%",
+					max_height = "60%",
+					prompt_position = "bottom",
+					reverse = false,
+				})
+			)
 		)
 	end,
 }
