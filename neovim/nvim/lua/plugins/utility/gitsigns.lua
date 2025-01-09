52140138726bff850ffd81ce036e26b496098553
gauloish <125079293+gauloish@users.noreply.github.com>
2025-01-09 04:28:05 -0300

    refactor: better gitsings ui

--- neovim/nvim/lua/plugins/utility/gitsigns.lua
+++ neovim/nvim/lua/plugins/utility/gitsigns.lua
@@ -2,25 +2,25 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = "BufEnter",
 	config = function()
-		-- TODO: configure gitsigns correctly
 		require("gitsigns").setup({
 			signs = {
-				add = { text = " +" },
-				change = { text = " ~" },
-				delete = { text = " -" },
-				topdelete = { text = " ‾" },
-				changedelete = { text = " ~" },
-				untracked = { text = " :" },
+				add          = { text = "│" }, -- ┃ ▎
+				change       = { text = "│" },
+				delete       = { text = "_" },
+				topdelete    = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked    = { text = "┆" },
 			},
-			numhl = true,
-			linehl = true,
+			signs_staged = {
+				add          = { text = "│" },
+				change       = { text = "│" },
+				delete       = { text = "_" },
+				topdelete    = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked    = { text = "┆" },
+			},
+			sign_priority = 0,
 			current_line_blame = true,
-			current_line_blame_opts = {
-				virt_text = true,
-				virt_text_pos = "eol",
-				delay = 1000,
-				ignore_whitespace = false,
-			},
 			current_line_blame_formatter = "<author>, <author_time:%d/%m/%Y>: <summary>",
 			update_debounce = 200,
 			max_file_length = 50000,
@@ -31,12 +31,11 @@ return {
 				row = 0,
 				col = 1,
 			},
-			-- on_attach = function(_)
-				-- vim.keymap.set("<a-g>vh", ":Gitsigns preview_hunk<cr>", { desc = "Open Preview Hunk", silent = true })
-				-- vim.keymap.set("<a-g>ph", ":Gitsigns prev_hunk<cr>", { desc = "Jump to Previous Hunk", silent = true })
-				-- vim.keymap.set("<a-g>nh", ":Gitsigns next_hunk<cr>", { desc = "Jump to Next Hunk", silent = true })
-				-- vim.keymap.set("<a-g>lh", ":Gitsigns toggle_linehl<cr>", { desc = "Open Preview Hunk", silent = true })
-			-- end,
+			on_attach = function(_)
+				vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", { desc = "Open Preview Hunk", silent = true })
+				vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<cr>", { desc = "Jump to Previous Hunk", silent = true })
+				vim.keymap.set("n", "]h", ":Gitsigns next_hunk<cr>", { desc = "Jump to Next Hunk", silent = true })
+			end,
 		})
 	end
 }
